2ca37dc55d151558b61d6002cff2ccc32e80dc8d
Chen Meng <miaoyuxuan10@outlook.com>
2025-04-09 16:33:58 +0800

    fix: Fixed some bugs

--- shaders/general.frag.glsl
+++ shaders/general.frag.glsl
@@ -1,9 +1,7 @@
 #version 450 core
 
-layout(location = 0) in vec4 v_Color;
-
 layout(location = 0) out vec4 f_Color;
 
 void main() {
-    f_Color = vec4(1.0, 1.0, 1.0, 1.0);
+    f_Color = vec4(1.0, 0.0, 0.0, 1.0);
 }
--- shaders/general.vert.glsl
+++ shaders/general.vert.glsl
@@ -1,27 +1,11 @@
 #version 450 core
 
-//layout (location = 0) in vec3 Position;
-//layout (location = 1) in vec4 Color;
-
-//layout (binding = 0) uniform UniformBufferObject {
-//    mat4 u_MVPMatrix;
-//} ubo;
-
-layout (location = 0) out vec4 v_Color;
-
-vec4[] positions = vec4[3](
-    vec4(-0.5, -0.5, 0.0, 1.0),
-    vec4(0.5, -0.5, 0.0, 1.0),
-    vec4(0.0, 0.5, 0.0, 1.0)
-);
-
-vec4[] colors = vec4[3](
-    vec4(1.0, 0.0, 0.0, 1.0),
-    vec4(0.0, 1.0, 0.0, 1.0),
-    vec4(0.0, 0.0, 1.0, 1.0)
+vec2 positions[3] = vec2[](
+    vec2(0.0, -0.5),
+    vec2(0.5, 0.5),
+    vec2(-0.5, 0.5)
 );
 
 void main() {
-    gl_Position = positions[gl_VertexIndex];
-    v_Color = colors[gl_VertexIndex];
+    gl_Position = vec4(positions[gl_VertexIndex], 0.0, 1.0);
 }
